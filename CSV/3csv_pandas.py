f8d3652b12df66ca2736b50c97dea3339b1081f5
Martin <40288037+martinyang99@users.noreply.github.com>
2018-07-11 17:01:34 +0800

    Add files via upload

--- CSV/3csv_pandas.py
+++ CSV/3csv_pandas.py
@@ -9,3 +9,10 @@ Created on Tue Jul 10 23:10:19 2018
 #%%
 import pandas as pd
 import sys
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+data_frame = pd.read_csv(inputfile)
+data_frame['Cost'] = data_frame['Cost'].str.strip('$').astype(float)
+data_frame_value_meets_condition = data_frame.loc[(data_frame['Supplier Name']\
+.str.contains('Z')) | (data_frame['Cost'] > 600.0), :]
+data_frame_value_meets_condition.to_csv(outputfile, index=False)

b7a7699b45453846c5dda2a28c79efec311274a1
Martin <40288037+martinyang99@users.noreply.github.com>
2018-07-26 16:59:55 +0800

    Add files via upload

--- excel/1excel.py
+++ excel/1excel.py
@@ -1,1 +1,17 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 26 16:44:25 2018
 
+@author: martin
+"""
+
+#%%
+import sys
+from xlrd import open_workbook
+input_file = sys.argv[1]
+workbook = open_workbook(input_file)
+print('Number of Worksheets:', workbook.nsheets)
+for worksheet in workbook.sheets():
+    print('Worksheet name:', worksheet.name, '\tRows:', worksheet.nrows, \
+          '\tColumns:', worksheet.ncols )

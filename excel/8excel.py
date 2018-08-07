f9c77a36b3b085366787024270af89fd71feaf59
Martin <40288037+martinyang99@users.noreply.github.com>
2018-08-07 17:56:59 +0800

    Add files via upload

--- excel/8excel.py
+++ excel/8excel.py
@@ -13,3 +13,15 @@ from xlrd import open_book, xldate_as_tuple
 from xlwt import Workbook
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+output_workbook = Workbook()
+output_worsheet = output_workbook.add_sheet('jan_2013_output')
+my_columns = ['Customer ID', 'Purchase Date']
+with open_book(input_file) as workbook:
+    worksheet = workbook.sheet_by_name('january_2013')
+    data = [my_columns]
+    header_list = worksheet.row_values(0)
+    header_index_list = []
+    for header_index in range(len(header_list)):
+        if header_list[header_index] in my_columns:
+            header_index_list.append(header_index)
+            

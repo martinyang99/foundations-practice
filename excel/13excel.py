86e1fa3b71c0833b74184ac18337b4023038265f
Martin <40288037+martinyang99@users.noreply.github.com>
2018-08-15 00:09:44 +0800

    Add files via upload

--- excel/13excel.py
+++ excel/13excel.py
@@ -24,21 +24,21 @@ for input_file in glob.glob(os.path.join(input_folder, '*.xlsx')):
     with open_workbook(input_file) as workbook:
         for worksheet in workbook.sheets():
             if first_worksheet:
-                header = worksheet.row_values(0)
-                data.append(header)
+                header_row = worksheet.row_values(0)
+                data.append(header_row)
                 first_worksheet = False
-        for row_index in range(1, worksheet.nrows):
-            row_list = []
-            for column_index in range(worksheet.ncols):
-                cell_value = worksheet.cell_value(row_index, column_index)
-                cell_type = worksheet.cell_type(row_index, column_index)
-                if cell_type == 3:
-                    date_cell = xldate_as_tuple(cell_value, workbook.datemode)
-                    date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
-                    row_list.append(date_cell)
-                else:
-                    row_list.append(cell_value)
-            data.append(row_list)
+            for row_index in range(1, worksheet.nrows):
+                row_list = []
+                for column_index in range(worksheet.ncols):
+                    cell_value = worksheet.cell_value(row_index, column_index)
+                    cell_type = worksheet.cell_type(row_index, column_index)
+                    if cell_type == 3:
+                        date_cell = xldate_as_tuple(cell_value, workbook.datemode)
+                        date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
+                        row_list.append(date_cell)
+                    else:
+                        row_list.append(cell_value)
+                data.append(row_list)
 for list_index, output_list in enumerate(data):
     for element_index, element in enumerate(output_list):
         output_worksheet.write(list_index, element_index, element)
--- excel/13pandas.py
+++ excel/13pandas.py
@@ -13,11 +13,14 @@ import os
 import glob
 input_path = sys.argv[1]
 output_file = sys.argv[2]
-all_workbooks = glob.glob(os.path.join(input_path), '*.xlsx')
+all_workbooks = glob.glob(os.path.join(input_path, '*.xlsx'))
 df = []
 for workbook in all_workbooks:
-    all_worksheets = pd.read_excel(workbook, index_col=False)
+    all_worksheets = pd.read_excel(workbook, sheet_name=None, index_col=False)
     for worksheet_name, data in all_worksheets.items():
         df.append(data)
-        
-        
+all_data_concatenated = pd.concat(df, axis=0, ignore_index=True)
+writer = pd.ExcelWriter(output_file)
+all_data_concatenated.to_excel(writer, sheet_name='all_data_all_workbooks',\
+                               index=False)
+writer.save()
